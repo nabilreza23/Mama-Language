cfd984e53ab1d532b24841731261e99d34009afb
Nabil Reza <161107779+nabilreza23@users.noreply.github.com>
2026-08-19 18:40:13 +0600

    Enhance setup.py with better project descriptions
    Updated project description and added long description support.

--- setup.py
+++ setup.py
@@ -1,15 +1,24 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="mama-lang",
     version="1.0.0",
+    description="Mama Language - An intuitive Bengali keyword-based programming language interpreter",
+    long_description=open("README.md").read() if os.path.exists("README.md") else "",
+    long_description_content_type="text/markdown",
     author="Nabil Reza",
-    description="The easiest and coolest programming language in the world!",
+    url="https://github.com/nabilreza23/Mama-Language",
     py_modules=["mama"],
+    install_requires=[],
     entry_points={
         "console_scripts": [
             "mama=mama:main",
         ],
     },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     python_requires=">=3.8",
 )
